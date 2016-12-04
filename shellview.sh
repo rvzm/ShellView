d53a2ed5806bf93e2e35e2f3ac807cef2cdd5543
rvzm <rvzm@protonmail.com>
2016-12-04 02:47:12 -0900

    small update

--- shellview.sh
+++ shellview.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # ShellView v0.3
-has-a2=$(dpkg-query -W --showformat='${status}\n' apache2|grep "install ok installed")
-has-f2b=$(dpkg-query -W --showformat='${Status}\n' fail2ban|grep "install ok installed")
+_has-a2=$(dpkg-query -W --showformat='${status}\n' apache2|grep "install ok installed")
+_has-f2b=$(dpkg-query -W --showformat='${Status}\n' fail2ban|grep "install ok installed")
 dialog --backtitle "ShellView" \
 	--title "ShellView 0.3" \
-	--menu "Please select a view" 20 100 11 \
+	--menu "Please select a view" 20 100 12 \
+	failedconn "SSH: Failed connect attempts" \
 	bruteforce "SSH: Brute-force attempts" \
-	fconn "SSH: Failed connections" \
 	fail2ban-warn "Fail2ban: Log WARN" \
         fail2ban-info "Fail2ban: Log INFO" \
         fail2ban-all "Fail2ban: Log ALL" \
@@ -19,8 +19,8 @@ dialog --backtitle "ShellView" \
 	2>temp
 if [ "$?" = "0" ]
 then
-        _return=$(cat temp)
-	if [ "$_return" = "fconn" ]
+	_return=$(cat temp)
+	if [ "$_return" = "failedconn" ]
 	then
 		~/shellview/check/ssh/fconn.sh
 	fi
@@ -28,7 +28,7 @@ then
 	then
 		~/shellview/check/ssh/brute.sh
 	fi
-	if [ "$has-f2b" != "" ]
+	if [ "$_has-f2b" != "" ]
 	then
 		if [ "$_return" = "fail2ban-warn" ]
 		then
@@ -47,7 +47,7 @@ then
 			--title "Error" \
 			--msgbox "fail2ban isnt installed" 15 150
 	fi
-	if [ "$has-a2" != "" ]; then
+	if [ "$_has-a2" != "" ]; then
 		if [ "$_return" = "apache2error" ]
 		then
 			~/shellview/check/apache2/error.sh
@@ -77,8 +77,6 @@ then
 	then
 		~/shellview/tools/about.sh
 	fi
-else
-	echo "Error: canceled."
 fi
 
 rm -f temp

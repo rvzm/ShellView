0e00518a266219f8792b68953823ccdd92820bb7
rvzm <rvzm@protonmail.com>
2016-12-05 03:24:50 -0900

    major update

--- shellview.sh
+++ shellview.sh
@@ -4,27 +4,20 @@ _has-a2=$(dpkg-query -W --showformat='${status}\n' apache2|grep "install ok inst
 _has-f2b=$(dpkg-query -W --showformat='${Status}\n' fail2ban|grep "install ok installed")
 dialog --backtitle "ShellView" \
 	--title "ShellView 0.4" \
-	--menu "Please select a view" 20 100 12 \
-	failedconn "SSH: Failed connect attempts" \
-	bruteforce "SSH: Brute-force attempts" \
+	--cancel-button exit \
+	--menu "Please select a view" 20 100 5 \
+	ssh "SSH" \
 	fail2ban "Fail2ban" \
-	apache2error "Apache2: Error log" \
-	apache2access "Apache2: Access log" \
-	apache2enabled "Apache2: Enabled sites" \
-	sysload "System: Load" \
-	sysdrive "System: Drive space" \
+	apache2 "Apache2" \
+	system "System" \
 	about "About ShellView" \
 	2>temp
 if [ "$?" = "0" ]
 then
 	_return=$(cat temp)
-	if [ "$_return" = "failedconn" ]
+	if [ "$_return" = "ssh" ]
 	then
-		~/shellview/check/ssh/fconn.sh
-	fi
-	if [ "$_return" = "bruteforce" ]
-	then
-		~/shellview/check/ssh/brute.sh
+		~/shellview/check/ssh/ssh.sh
 	fi
 	if [ "$_has-f2b" != "" ]
 	then
@@ -38,30 +31,18 @@ then
 			--msgbox "fail2ban isnt installed" 15 150
 	fi
 	if [ "$_has-a2" != "" ]; then
-		if [ "$_return" = "apache2error" ]
+		if [ "$_return" = "apache2" ]
 		then
-			~/shellview/check/apache2/error.sh
-		fi
-		if [ "$_return" = "apache2access" ]
-		then
-			~/shellview/check/apache2/access.sh
-		fi
-		if [ "$_return" = "apache2enabled" ]
-		then
-			~/shellview/check/apache2/enabled.sh
+			~/shellview/check/apache2/apache2.sh
 		fi
 	else
 		dialog --backtitle "ShellView" \
 			--title "Error" \
 			--msgbox "Apache2 isnt installed" 15 150
 	fi
-	if [ "$_return" = "sysload" ]
+	if [ "$_return" = "system" ]
 	then
-		~/shellview/tools/sysload.sh
-	fi
-	if [ "$_return" = "sysdrive" ]
-	then
-		~/shellview/tools/sysdrive.sh
+		~/shellview/tools/system.sh
 	fi
 	if [ "$_return" = "about" ]
 	then
@@ -71,3 +52,4 @@ fi
 
 rm -f temp
 clear
+echo "ShellView exited"
